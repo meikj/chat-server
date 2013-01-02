56efe7793acd72f1374f537c43b0242a496fdba7
John Meikle <john.meikle31@gmail.com>
2013-01-02 19:09:52 +0000

    Started refactoring some clean up code
    Created a generic cleanup() function which should be called when clean
    up is required, especially in the event of a fatal error. This should
    make things tidier

--- src/main.c
+++ src/main.c
@@ -3,6 +3,17 @@
 
 #include "logger.h"
 
+void cleanup(int sockets[], int socket_count) {
+	if((socket_count == 0) || (sockets == NULL)) {
+		// No need to cleanup any sockets
+		
+	}
+	if(WSACleanup() == SOCKET_ERROR) {
+		// WSACleanup() failed
+		
+	}
+}
+
 int main(void) {
 	WSADATA wsaData;
 	int error;
@@ -19,13 +30,30 @@ int main(void) {
 	
 	log_entry("WSAStartup() succeeded", 0, LOGGER_DEBUG);
 	
-	if((l_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
-		// Some error occured with socket()
+0	if((l_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {		// Some error occured with socket()
 		log_entry("socket() failed", WSAGetLastError(), LOGGER_ERROR);
+		WSACleanup();
 		exit(1);
 	}
 	
 	log_entry("socket() succeeded", l_socket, LOGGER_DEBUG);
+	
+	if(closesocket(l_socket) == SOCKET_ERROR) {
+		// Closing socket resulted in an error
+		log_entry("closesocket() failed", WSAGetLastError(), LOGGER_ERROR);
+		WSACleanup();
+		exit(1);
+	}
+	
+	log_entry("closesocket() succeeded", 0, LOGGER_DEBUG);
+	
+	if(WSACleanup() == SOCKET_ERROR) {
+		// Cleanup resulted in an error
+		log_entry("WSACleanup() failed", WSAGetLastError(), LOGGER_ERROR);
+		exit(1);
+	}
+	
+	log_entry("WSACleanup() succeeded", 0, LOGGER_DEBUG);
 
 	return 0;
 }
