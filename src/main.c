367d8354a88024dafd9b4e570df7cbff0f596300
John Meikle <john.meikle31@gmail.com>
2013-01-02 18:19:13 +0000

    Begun socket code
    Wrote the WSAStartup code and initialised the listen socket. Some error
    checking has been included

--- src/main.c
+++ src/main.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <winsock.h>
 
 int main(void) {
+	WSAData wsaData;
+	int error;
+	
+	int l_socket;
+	
+	error = WSAStartup(MAKEWORD(1, 1), &wsaData);
+	
+	if(error != 0) {
+		// Some error occured with WSAStartup(), therefore we can't continue
+		printf("WSAStartup() failed with error code: %d\n", error);
+		exit(1);
+	}
+	
+	if((l_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+		// Some error occured with socket()
+		printf("socket() failed with error code: %d\n", WSAGetLastError());
+		exit(1);
+	}
+
 	return 0;
 }
