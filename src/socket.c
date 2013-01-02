bfd4cb8564fb7806f7fdaa1d12f12ca230a1b2b3
John Meikle <john.meikle31@gmail.com>
2013-01-02 22:53:14 +0000

    Added WinSock version definitions and checking
    Checking has been put in place to ensure that the received WinSock
    library is the version as requested. Definitions have been made to allow
    the requested version of the WinSock library to be changed (default is
    1.1)

--- src/socket.c
+++ src/socket.c
@@ -42,24 +42,36 @@ int init_socket() {
 	int error;
 	int l_socket;
 	
-	error = WSAStartup(MAKEWORD(1, 1), &wsaData);
+	error = WSAStartup(MAKEWORD(WINSOCK_MAJOR, WINSOCK_MINOR), &wsaData);
 	
 	if(error != 0) {
 		// Some error occured with WSAStartup(), therefore we can't continue
-		log_entry(LOGGER_ERROR, error, "main(): WSAStartup() failed");
+		log_entry(LOGGER_ERROR, error, "init_socket(): WSAStartup() failed");
 		exit(1);
 	}
 	
-	log_entry(LOGGER_DEBUG, 0, "main(): WSAStartup() succeeded with WinSock 1.1");
+	// Check if WinSock version conforms to the requested version
+	if(LOBYTE(wsaData.wVersion) != WINSOCK_MAJOR || HIBYTE(wsaData.wVersion) != WINSOCK_MINOR) {
+		// Version wasn't found, therefore library isn't compatible
+		log_entry(LOGGER_ERROR, 0, 
+			"init_socket(): WinSock lib requested wasn't found: requested = %d.%d, received = %d.%d",
+			WINSOCK_MAJOR, WINSOCK_MINOR,
+			LOBYTE(wsaData.wVersion), HIBYTE(wsaData.wVersion));
+			
+		exit(1);
+	}
+	
+	log_entry(LOGGER_DEBUG, 0, "init_socket(): WSAStartup() succeeded with WinSock %d.%d lib",
+		WINSOCK_MAJOR, WINSOCK_MINOR);
 	
 	if((l_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {		
 		// Some error occured with socket()
-		log_entry(LOGGER_ERROR, WSAGetLastError(), "main(): socket() failed");
+		log_entry(LOGGER_ERROR, WSAGetLastError(), "init_socket(): socket() failed");
 		cleanup(0, 1); // No need to cleanup socket as it failed
 		exit(1);
 	}
 	
-	log_entry(LOGGER_DEBUG, l_socket, "main(): socket() succeeded");
+	log_entry(LOGGER_DEBUG, l_socket, "init_socket(): socket() succeeded");
 	
 	return l_socket;
 }
--- src/socket.h
+++ src/socket.h
@@ -2,6 +2,9 @@
 	
 	#define SOCKET_H
 	
+	#define WINSOCK_MAJOR 1
+	#define WINSOCK_MINOR 1
+	
 	void cleanup(int socket, int wsa_cleanup);
 	int init_socket();
 	
