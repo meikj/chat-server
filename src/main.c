923de02639b268b7770a7305c7e79fe12915053a
John Meikle <john.meikle31@gmail.com>
2013-07-22 15:17:57 +0100

    Slightly improved socket comments

--- src/main.c
+++ src/main.c
@@ -27,67 +27,68 @@ int main(int argc, char *argv[]) {
 		printf("Usage: %s <host> <port_number>\n", argv[0]);
 		exit(1);
 	}
-	
-	// Socket variables
+
+	// Listen socket variables
 	int l_socket;
 	char *l_host;
 	int l_port;
 	struct sockaddr_in l_addr;
-	
+
+	// Client socket variables
 	int c_socket;
-	
+
 	l_socket = init_socket();
 	l_host = argv[1];
 	l_port = atoi(argv[2]);
-	
-	// Do some port checking
+
+	// Do some primitive port checking
 	if(l_port == 0) {
 		log_entry(LOGGER_ERROR, 0, "main(): invalid port specified");
 		cleanup(l_socket, 1);
 		exit(1);
 	}
-	
+
 	// Output address and port for debugging purposes
 	log_entry(LOGGER_DEBUG, 0, "main(): l_host = %s", l_host);
 	log_entry(LOGGER_DEBUG, 0, "main(): l_port = %d", l_port);
-	
+
 	// Create address structure and bind socket
 	l_addr = init_server_addr(l_host, l_port);
-	
+
 	if((bind(l_socket, (struct sockaddr *)&l_addr, sizeof l_addr)) == SOCKET_ERROR) {
 		log_entry(LOGGER_ERROR, WSAGetLastError(), "main(): bind() failed");
 		cleanup(l_socket, 1);
 		exit(1);
 	}
-	
+
 	log_entry(LOGGER_DEBUG, 0, "main(): bind() was successful: address = %s, port = %i",
 		l_host, l_port);
-		
+
 	if(listen(l_socket, MAX_BACKLOG) == SOCKET_ERROR) {
 		log_entry(LOGGER_ERROR, WSAGetLastError(), "main(): listen() failed");
 		cleanup(l_socket, 1);
 		exit(1);
 	}
-	
+
 	log_entry(LOGGER_DEBUG, 0, "main(): listen() was successful: socket = %d, backlog = %d",
 		l_socket, MAX_BACKLOG);
-		
+
 	// Wait for connection
 	log_entry(LOGGER_INFO, 0, "main(): waiting for connection(s)...");
-	
+
 	for(;;) {
 		// Main program loop
 		c_socket = accept(l_socket, NULL, NULL);
-		
+
 		if(c_socket == INVALID_SOCKET) {
 			log_entry(LOGGER_ERROR, WSAGetLastError(), "main(): accept() failed");
 			cleanup(l_socket, 1);
 			exit(1);
 		}
-		
+
 		log_entry(LOGGER_INFO, 0, "main(): accept() was successful: c_socket = %d", c_socket);
 	}
-	
+
 	// Perform final clean up code
 	cleanup(l_socket, 1);
 
