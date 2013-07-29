27c9f58b8e11ba778e96a7c8fa9b8408370b4ebf
John Meikle <john@meikj.com>
2013-07-30 00:40:41 +0100

    Initial commit for protocol

---
+++ src/protocol.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void protocol_parse(const char *message, const size_t len) {
+	return;
+}
---
+++ src/protocol.h
@@ -0,0 +1,5 @@
+#ifndef PROTOCOL_H
+
+	void protocol_parse(const char *message, const size_t len);
+
+#endif
